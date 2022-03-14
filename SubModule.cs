e02efed02feddafe66d72f0c16a908f018659f31
Lesser Scholar <lesserscholar@gmail.com>
2022-03-14 17:12:35 +0200

    Updated for 1.7.2. Bump version to 0.1.1

--- SubModule.cs
+++ SubModule.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using TaleWorlds.CampaignSystem;
 using TaleWorlds.CampaignSystem.Actions;
+using TaleWorlds.CampaignSystem.Settlements;
 using TaleWorlds.Library;
 using TaleWorlds.MountAndBlade;
 using TaleWorlds.ObjectSystem;
