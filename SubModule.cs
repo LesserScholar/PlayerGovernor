7525d37d16a036af20d99cf8e7a4e27d504a3bff
Lesser Scholar <lesserscholar@gmail.com>
2022-03-24 11:37:31 +0200

    Fix format

--- SubModule.cs
+++ SubModule.cs
@@ -34,44 +34,45 @@ namespace PlayerGovernor
         }
 
 
-		[CommandLineFunctionality.CommandLineArgumentFunction("get_current_governor", "player_governor")]
-		public static string GiveSettlementToPlayer(List<string> strings)
-		{
-			if (CampaignCheats.CheckParameters(strings, 0))
-			{
-				return "Format is \"player_governor.get_current_governor [SettlementName/SettlementId]";
-			}
-			string text = CampaignCheats.ConcatenateString(strings);
+        [CommandLineFunctionality.CommandLineArgumentFunction("get_current_governor", "player_governor")]
+        public static string GiveSettlementToPlayer(List<string> strings)
+        {
+            if (CampaignCheats.CheckParameters(strings, 0))
+            {
+                return "Format is \"player_governor.get_current_governor [SettlementName/SettlementId]";
+            }
+            string text = CampaignCheats.ConcatenateString(strings);
 
-			string text3 = text;
-			Settlement settlement2 = MBObjectManager.Instance.GetObject<Settlement>(text3);
-			if (settlement2 == null)
-			{
-				foreach (Settlement settlement3 in MBObjectManager.Instance.GetObjectTypeList<Settlement>())
-				{
-					if (settlement3.Name.ToString().Equals(text3, StringComparison.InvariantCultureIgnoreCase))
-					{
-						settlement2 = settlement3;
-						break;
-					}
-				}
-			}
-			if (settlement2 == null)
-			{
-				return "Given settlement name or id could not be found.";
-			}
+            string text3 = text;
+            Settlement settlement2 = MBObjectManager.Instance.GetObject<Settlement>(text3);
+            if (settlement2 == null)
+            {
+                foreach (Settlement settlement3 in MBObjectManager.Instance.GetObjectTypeList<Settlement>())
+                {
+                    if (settlement3.Name.ToString().Equals(text3, StringComparison.InvariantCultureIgnoreCase))
+                    {
+                        settlement2 = settlement3;
+                        break;
+                    }
+                }
+            }
+            if (settlement2 == null)
+            {
+                return "Given settlement name or id could not be found.";
+            }
             var gov = settlement2.Town.Governor;
             if (gov == null)
             {
                 return "No governor appointed.";
             }
-			return settlement2.Town.Governor.Name.ToString();
-		}
-	}
+            return settlement2.Town.Governor.Name.ToString();
+        }
+    }
 
     [HarmonyPatch(typeof(Town), "Governor", MethodType.Getter)]
 
-    public class TownPatch {
+    public class TownPatch
+    {
 
         public static void Postfix(ref Hero __result, Town __instance)
         {
